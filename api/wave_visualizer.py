1ca8bd5aabbba12dd2ac96f9bd3dd2e6c55bcca1
Kareem Halabi <kareemhalabi24@gmail.com>
2018-11-29 22:53:00 -0500

    Add extra parameter for changing plot title

--- api/wave_visualizer.py
+++ api/wave_visualizer.py
@@ -17,7 +17,8 @@ aeval = Interpreter(syms)
 
 _CONTINUOUS_STEPS = 1000
 
-def visualize_wave(equation_str: str, variable: str, var_range: Tuple[float, float], discretization_step: float = None):
+def visualize_wave(equation_str: str, variable: str, var_range: Tuple[float, float], discretization_step: float = None,
+                   wave_title: str = None):
     """
     Displays a plot for visualizing a continuous function over a small period of time. Optionally can display
     discretization as an overlayed plot
@@ -26,7 +27,11 @@ def visualize_wave(equation_str: str, variable: str, var_range: Tuple[float, flo
     :param variable:  String representing single variable
     :param var_range:  Range of function to plot
     :param discretization_step: Optionally display discretization steps
+    :param wave_title: Optionally change the title of the plot. Default is equation_str
     """
+
+    if wave_title is None:
+        wave_title = 'Visualization of %s' % equation_str
 
     # Continuous first
     aeval.symtable[variable] = np.arange(var_range[0], var_range[1], (var_range[1]-var_range[0])/_CONTINUOUS_STEPS)
@@ -35,8 +40,8 @@ def visualize_wave(equation_str: str, variable: str, var_range: Tuple[float, flo
     cont_func = aeval.symtable['cont_func']
 
     fig, ax = plt.subplots()
-    ax.plot(cont_var, cont_func, label='Continuous')
-    ax.set(xlabel='Time (s)', ylabel='Current (A)', title='Visualization of %s' % equation_str)
+    ax.plot(cont_var, cont_func, label='Desired')
+    ax.set(xlabel='Time (s)', ylabel='Current (A)', title=wave_title)
     ax.grid()
 
     if discretization_step is not None:
@@ -45,7 +50,7 @@ def visualize_wave(equation_str: str, variable: str, var_range: Tuple[float, flo
         disc_var = aeval.symtable[variable]
         aeval('disc_func = %s' % equation_str)
         disc_func = aeval.symtable['disc_func']
-        plt.step(disc_var, disc_func, where='post', label='Discrete')
+        plt.step(disc_var, disc_func, where='post', label='Actual')
         plt.legend()
 
     plt.show()
